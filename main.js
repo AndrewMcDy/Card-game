2d82fc5c9a4a3c4e79c5320213e0bd1788f1282f
Andrew Borysko <andrewmcdy@gmail.com>
2022-03-25 13:45:29 +0200

    working card logic

--- main.js
+++ main.js
@@ -2,16 +2,16 @@
 
     let anyArray = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
     let variable = "0";
-    
+
     function shuffleCards(array) {
-        // let m = array.length, t, i;
-        // while (m) {
-        //     i = Math.floor(Math.random() * m--);
-        //     t = array[m];
-        //     array[m] = array[i];
-        //     array[i] = t;
-        // }
-     return array;
+        let m = array.length, t, i;
+        while (m) {
+            i = Math.floor(Math.random() * m--);
+            t = array[m];
+            array[m] = array[i];
+            array[i] = t;
+        }
+        return array;
     }
 
     function createAppTitle() {
@@ -22,10 +22,10 @@
     }
 
     function createAppControls() {
-        let appControls = document.createElement('div');        
-        let score = document.createElement('div');        
-        let buttonsWrapper = document.createElement('div');        
-        let startButton = document.createElement('button');        
+        let appControls = document.createElement('div');
+        let score = document.createElement('div');
+        let buttonsWrapper = document.createElement('div');
+        let startButton = document.createElement('button');
         let stopButton = document.createElement('button');
 
         appControls.classList.add('card-game-app__controls');
@@ -33,21 +33,18 @@
         buttonsWrapper.classList.add('card-game-app__buttons-wrapper');
         startButton.classList.add('card-game-app__btn', 'card-game-app__btn_start');
         stopButton.classList.add('card-game-app__btn', 'card-game-app__btn_stop', 'disabled');
-
         startButton.textContent = "Start Game";
         stopButton.textContent = "Stop Game";
         score.textContent = "0";
-
         buttonsWrapper.append(startButton);
         buttonsWrapper.append(stopButton);
         appControls.append(score);
         appControls.append(buttonsWrapper);
-
         return {
             appControls,
             startButton,
             stopButton,
-        }    
+        }
     }
 
     function createGameField(cardsNumber = 16) {
@@ -55,7 +52,7 @@
         appField.classList.add('card-game-app__game-field');
 
         array = shuffleCards(anyArray);
-        
+
         for (let i = 0; i < cardsNumber; i++) {
             let card = document.createElement('div');
             let cardFront = document.createElement('div');
@@ -68,14 +65,12 @@
             card.append(cardBack);
             appField.append(card);
         }
-
         return appField;
     }
-
     function createApp(container) {
 
         let gameTitle = createAppTitle();
-        let gameControls= createAppControls();
+        let gameControls = createAppControls();
         let gameField = createGameField(anyArray.length);
 
         container.append(gameTitle);
@@ -83,44 +78,46 @@
         container.append(gameField);
 
         let cards = document.querySelectorAll(".card-game-app__card");
+
+        function onClick(event) {
+            const item = event.target.closest(".card-game-app__card");
+            item.classList.toggle('card-game-app__card-rotate');
+
+            if (variable === "0") {
+                variable = item.textContent;
+            } else {
+                if (variable === item.textContent) {
+                    let cards = document.querySelectorAll('.card-game-app__card');
+                    cards.forEach(card => {
+                        if (card.textContent === variable) {
+                            card.removeEventListener('click', onClick);
+                        }
+                    })
+                    variable = "0";
+                } else {
+                    setTimeout(function () {
+                        item.classList.remove('card-game-app__card-rotate');
+                        let cards = document.querySelectorAll('.card-game-app__card');
+                        cards.forEach(itemN => {
+                            if (itemN.textContent === variable) {
+                                itemN.classList.remove('card-game-app__card-rotate');
+                            }
+                        });
+                        variable = "0";
+                    }, 1000)
+                }
+            }
+        }
+
+
         cards.forEach(item => {
 
             item.addEventListener('click', onClick);
-
-            function onClick(event) {
-                item.classList.toggle('card-game-app__card-rotate');
-
-                if (variable === "0") {
-                    variable = item.textContent;
-                } else {
-                    if (variable === item.textContent) {
-                        let cards = document.querySelectorAll('.card-game-app__card');
-                        cards.forEach(item => {
-                            if (item.textContent === variable) {
-                                console.log(item);
-                                item.removeEventListener('click', onClick);
-                            }
-                        })
-                        variable = "0";
-                    } else {                        
-                        setTimeout(function () {                            
-                            item.classList.remove('card-game-app__card-rotate');
-                            let cards = document.querySelectorAll('.card-game-app__card');
-                            cards.forEach(itemN => {
-                                if (itemN.textContent === variable) {
-                                    itemN.classList.remove('card-game-app__card-rotate');
-                                }
-                            });
-                            variable = "0";
-                        }, 1000)                        
-                    }
-                }            
-            }            
         });
     }
 
     document.addEventListener('DOMContentLoaded', function () {
         createApp(document.getElementById('card-game-app'));
     });
-    
+
 })();
